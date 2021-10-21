3b5362cd7e1e9fd99198d5f66ae1d39ff9065340
lydiandylin <lydiandylin@gmail.com>
2021-10-22 00:52:41 +0800

    update to latest version of V

--- test/update_test.v
+++ test/update_test.v
@@ -4,34 +4,34 @@ pub fn test_inert_update_delete() {
 	mut db := connect_and_init_db()
 	mut res := ''
 	// insert
-	res = db.table('person').insert(map{
-		'id':   '255'
-		'name': 'abc'
+	res = db.table('person').insert({
+		'id':   '255',
+		'name': 'abc',
 		'age':  '36'
 	}).to_sql()
 	assert res == "insert into person (id,name,age) values ('255','abc','36')"
 
-	res = db.table('person').insert(map{
-		'id':   '255'
-		'name': 'abc'
+	res = db.table('person').insert({
+		'id':   '255',
+		'name': 'abc',
 		'age':  '36'
 	}).returning('id', 'name').to_sql()
 	assert res == "insert into person (id,name,age) values ('255','abc','36') returning id,name"
 
-	res = db.insert(map{
-		'id':   '12'
-		'name': 'tom'
+	res = db.insert({
+		'id':   '12',
+		'name': 'tom',
 	}).into('person').returning('id').to_sql()
 	assert res == "insert into person (id,name) values ('12','tom') returning id"
 
 	// update
-	res = db.table('person').update(map{
+	res = db.table('person').update({
 		'name': 'paris'
 	}).where('id=1').returning('id').to_sql()
 	assert res == "update person set name='paris' where (id=1) returning id"
 
-	res = db.table('person').update(map{
-		'name': 'paris'
+	res = db.table('person').update({
+		'name': 'paris',
 		'age':  '32'
 	}).where('id=1').returning('id').to_sql()
 	assert res == "update person set name='paris',age='32' where (id=1) returning id"
